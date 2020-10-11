4ae1165b2372183ef25f90223e82b497542f9099
Naveen Rawat <naveen.rawat@sprint.com>
2020-10-11 12:32:25 +0530

    logo search box and header links added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import './App.less';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/checkout"><h1>Checkout</h1></Route>
-          <Route path="/login"><h1>login</h1></Route>
-          <Route path="/"><Header/></Route>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>login</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+          </Route>
         </Switch>
       </Router>
     </div>
